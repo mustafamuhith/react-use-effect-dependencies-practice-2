3a0c5f1e4ac780160ec9e2fbcc856bda5bb818f7
Mustafa <mustafamuhith@gmail.com>
2022-11-16 15:26:55 +0000

    core

--- src/components/StarshipsListItem.js
+++ src/components/StarshipsListItem.js
@@ -8,10 +8,16 @@ function StarshipsListItem(props) {
   console.log({ starship });
 
   useEffect (() => {
-    fetch(starship.name[0])
+    if (starship.pilots.length === 0) {
+      setFirstPilot('unknown')
+       return
+      }
+
+    fetch(starship.pilots[0])
     .then((res) => res.json())
     .then((pilotData) => {
       console.log(pilotData)
+      setFirstPilot(pilotData.name)
     })
   }, [starship])
   return <li>{starship.name} - First Pilot {firstPilot}</li>;
